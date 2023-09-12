803d640df21bb1dfea2df1b37ae6377d70f04366
SHAKOTN <shakotn@protonmail.com>
2023-09-12 19:32:21 +0300

    test: add first basic test for deposits

--- test/BaseFixture.sol
+++ test/BaseFixture.sol
@@ -7,15 +7,19 @@ import { Vault } from "../src/Vault.sol";
 
 import "forge-std/Test.sol";
 import "./Utils.sol";
+import "../lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 
 contract BaseFixture is Test {
     using stdStorage for StdStorage;
+
+    ERC20 public AURA = ERC20(address(0xC0c293ce456fF0ED870ADd98a0828Dd4d2903DBF));
 
     Utils internal utils;
     address payable[] internal users;
     address public alice;
     address public bob;
     address public basedAdmin;
+    address public governance;
     AdminUpgradeabilityProxy public goldAURAProxy;
     AdminUpgradeabilityProxy public auraStrategyProxy;
     Vault public vaultImpl;
@@ -34,21 +38,22 @@ contract BaseFixture is Test {
         // https://etherscan.io/block/18090274
         vm.createSelectFork("mainnet", 18_090_274);
         utils = new Utils();
-        users = utils.createUsers(3);
+        users = utils.createUsers(4);
         alice = users[0];
         vm.label(alice, "Alice");
         bob = users[1];
         vm.label(bob, "Bob");
         basedAdmin = users[2];
         vm.label(basedAdmin, "Based Admin");
+        governance = users[3];
+        vm.label(governance, "Governance");
         uint256[4] memory _feeConfig = [uint256(0), uint256(0), uint256(0), uint256(0)];
-        address _token = address(0xC0c293ce456fF0ED870ADd98a0828Dd4d2903DBF);
         string memory _name = "Gold Aura";
         string memory _symbol = "gAURA";
         bytes memory initData = abi.encodeWithSignature(
             "initialize(address,address,address,address,address,address,string,string,uint256[4])",
-            _token,
-            address(1337), // governance
+            address(AURA),
+            governance, // governance
             address(1337), //  keeper
             address(1337), // guardian
             address(1337),
@@ -67,5 +72,7 @@ contract BaseFixture is Test {
 
         auraStrategy = AuraStrategy(payable(auraStrategyProxy));
         vault = Vault(payable(goldAURAProxy));
+        vm.prank(governance);
+        vault.setStrategy(address(auraStrategy));
     }
 }
--- test/Vault.t.sol
+++ test/Vault.t.sol
@@ -10,6 +10,7 @@ contract TestVault is BaseFixture {
 
     function testSetup() public {
         assertEq(vault.symbol(), "gAURA");
+        assertEq(vault.strategy(), address(auraStrategy));
         assertEq(vault.totalSupply(), 0);
 
         assertEq(auraStrategy.getName(), "GOLD vlAURA Voting Strategy");
@@ -21,5 +22,26 @@ contract TestVault is BaseFixture {
         assertEq(auraStrategyProxy.admin(), basedAdmin);
         assertEq(auraStrategyProxy.implementation(), address(auraStrategyImpl));
         vm.stopPrank();
+    }
+
+    function testSimpleDeposit(uint256 _depositAmount) public {
+        vm.assume(_depositAmount > 1e18);
+        vm.assume(_depositAmount < 100_000_000e18);
+        // Give alice some AURA:
+        setStorage(alice, AURA.balanceOf.selector, address(AURA), _depositAmount);
+        vm.startPrank(alice);
+        // Approve the vault to spend AURA:
+        AURA.approve(address(vault), _depositAmount);
+        vault.deposit(_depositAmount);
+        vm.stopPrank();
+
+        // Make sure alice has shares now
+        assertEq(vault.balanceOf(alice), _depositAmount);
+        // As Alice holds 100% shares in vault, she has 100% shares as well:
+        assertEq(vault.balanceOf(alice) * vault.getPricePerFullShare() / 1e18, _depositAmount);
+        assertEq(vault.getPricePerFullShare(), 1e18);
+
+        // Check total supply should be equal to alice's balance
+        assertEq(vault.totalSupply(), _depositAmount);
     }
 }
