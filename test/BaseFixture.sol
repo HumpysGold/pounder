f5d97e78b2a727fbf121a2e49dd34c292a601041
SHAKOTN <shakotn@protonmail.com>
2023-09-12 17:28:51 +0300

    tests: finish configuring base fixture for testing

--- test/BaseFixture.sol
+++ test/BaseFixture.sol
@@ -15,10 +15,15 @@ contract BaseFixture is Test {
     address payable[] internal users;
     address public alice;
     address public bob;
+    address public basedAdmin;
+    AdminUpgradeabilityProxy public goldAURAProxy;
+    AdminUpgradeabilityProxy public auraStrategyProxy;
+    Vault public vaultImpl;
+    AuraStrategy public auraStrategyImpl;
 
-    AdminUpgradeabilityProxy public goldAURA;
-    Vault public vault;
+    // Exported for testing
     AuraStrategy public auraStrategy;
+    Vault public vault;
 
     function setStorage(address _user, bytes4 _selector, address _contract, uint256 value) public {
         uint256 slot = stdstore.target(_contract).sig(_selector).with_key(_user).find();
@@ -29,11 +34,13 @@ contract BaseFixture is Test {
         // https://etherscan.io/block/18090274
         vm.createSelectFork("mainnet", 18_090_274);
         utils = new Utils();
-        users = utils.createUsers(2);
+        users = utils.createUsers(3);
         alice = users[0];
         vm.label(alice, "Alice");
         bob = users[1];
         vm.label(bob, "Bob");
+        basedAdmin = users[2];
+        vm.label(basedAdmin, "Based Admin");
         uint256[4] memory _feeConfig = [uint256(0), uint256(0), uint256(0), uint256(0)];
         address _token = address(0xC0c293ce456fF0ED870ADd98a0828Dd4d2903DBF);
         string memory _name = "Gold Aura";
@@ -50,7 +57,15 @@ contract BaseFixture is Test {
             _symbol,
             _feeConfig
         );
-        vault = new Vault();
-        goldAURA = new AdminUpgradeabilityProxy(address(vault), address(this), initData);
+        vaultImpl = new Vault();
+        goldAURAProxy = new AdminUpgradeabilityProxy(address(vaultImpl), address(basedAdmin), initData);
+
+        // Deploy strategy
+        auraStrategyImpl = new AuraStrategy();
+        initData = abi.encodeWithSignature("initialize(address)", address(goldAURAProxy));
+        auraStrategyProxy = new AdminUpgradeabilityProxy(address(auraStrategyImpl), address(basedAdmin), initData);
+
+        auraStrategy = AuraStrategy(payable(auraStrategyProxy));
+        vault = Vault(payable(goldAURAProxy));
     }
 }
--- test/Vault.t.sol
+++ test/Vault.t.sol
@@ -3,13 +3,23 @@ pragma solidity 0.8.20;
 
 import "./BaseFixture.sol";
 
-
 contract TestVault is BaseFixture {
     function setUp() public override {
         super.setUp();
     }
 
     function testSetup() public {
-        assertEq(goldAURA.admin(), address(this));
+        assertEq(vault.symbol(), "gAURA");
+        assertEq(vault.totalSupply(), 0);
+
+        assertEq(auraStrategy.getName(), "GOLD vlAURA Voting Strategy");
+
+        vm.startPrank(basedAdmin);
+        assertEq(goldAURAProxy.admin(), basedAdmin);
+        assertEq(goldAURAProxy.implementation(), address(vaultImpl));
+
+        assertEq(auraStrategyProxy.admin(), basedAdmin);
+        assertEq(auraStrategyProxy.implementation(), address(auraStrategyImpl));
+        vm.stopPrank();
     }
 }
